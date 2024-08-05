4a9f924ea9e008882a84d7e7d17feaef24d0c84d
Adam Dryden <76844656+Phlotonic@users.noreply.github.com>
2024-08-05 00:31:07 -0500

    Submitting my first Coding Temple assignment on GitHub

---
+++ Showcase Your Dance Moves.py
@@ -0,0 +1,33 @@
+# Task 1: Below is a piece of Python code with incorrect indentation. Your task is to correct it.
+
+# weather = "sunny"
+
+# if weather == "sunny":
+# print("Wear sunglasses!")
+# else:
+# print("Take an umbrella!")
+
+# Answer: Indented the print statements.
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2: Your Mood Today 
+# Ask the user how they feel today. 
+# If they say "happy", print "That's great to hear!", 
+# and if they say "sad", print "I hope your day gets better!". 
+# Ensure your if statement is properly indented.
+
+# Answer:
+
+mood = input("How do you feel today?")
+if mood == "happy":
+    print("That's great to hear!")
+elif mood == "sad":
+    print("I hope your day gets better!")
+
+#
